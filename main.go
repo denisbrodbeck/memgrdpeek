68f8383745bd534817def97a8e06b043348ac773
Denis Brodbeck <denis@shahaya.com>
2017-10-21 13:17:00 +0200

    Simplify main app.
    Only use one message type.

--- main.go
+++ main.go
@@ -1,66 +1,52 @@
-package main
+package main // import "github.com/denisbrodbeck/memgrdpeek"
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/awnumar/memguard"
 )
 
-type publicMessage struct {
-	Pre  []byte // 32 byte
-	Mid  []byte // 32 byte
-	Post []byte // 32 byte
+type message struct {
+	ID        []byte                 // 32 byte
+	Recipient []byte                 // 32 byte
+	Sender    []byte                 // 32 byte
+	Message   *memguard.LockedBuffer // Gets 32 byte input
+	Meta      []byte                 // 32 byte
 }
 
-func newPublicMessage(pre, mid, post []byte) *publicMessage {
-	return &publicMessage{
-		Pre:  assert32Bytes(pre),
-		Mid:  assert32Bytes(mid),
-		Post: assert32Bytes(post),
-	}
-}
-
-type privateMessage struct {
-	Pre  []byte                 // 32 byte
-	Mid  *memguard.LockedBuffer // Gets 32 byte input
-	Post []byte                 // 32 byte
-}
-
-func newPrivateMessage(pre, mid, post []byte) *privateMessage {
-	middle, err := memguard.NewImmutableFromBytes(assert32Bytes(mid))
+func newMessage(id, recipient, sender, msg, meta []byte) *message {
+	msgBuf, err := memguard.NewImmutableFromBytes(assert32Bytes(msg))
 	if err != nil {
 		panic(err)
 	}
-	return &privateMessage{
-		Pre:  assert32Bytes(pre),
-		Mid:  middle,
-		Post: assert32Bytes(post),
+	return &message{
+		ID:        assert32Bytes(id),
+		Recipient: assert32Bytes(recipient),
+		Sender:    assert32Bytes(sender),
+		Message:   msgBuf,
+		Meta:      assert32Bytes(meta),
 	}
 }
 
 func main() {
 	defer memguard.DestroyAll()
-	publicMsg := newPublicMessage(
-		[]byte("(((pre| hi bob bob bobby |pre)))"),
-		[]byte("(((mid| public  message! |mid)))"),
-		[]byte("(((pos| after the data.. |pos)))"),
+	msg := newMessage(
+		[]byte("(((id    | 1234567890123456  )))"),
+		[]byte("(((recip | bobby@spacer.com  )))"),
+		[]byte("(((sender| eva@secretmoon.m  )))"),
+		[]byte("(((msg   | VERY SECRET VERY  )))"),
+		[]byte("(((meta  | 2017-10-21 10:26  )))"),
 	)
-	empty := []byte("                                ")
-	// create an empty msg as a memory separator (makes py script output easier to read)
-	emptyMsg := newPublicMessage(empty, empty, empty)
-	privateMsg := newPrivateMessage(
-		[]byte("(((pre| hi eva eva eva!! |pre)))"),
-		[]byte("(((mid| private message! |mid)))"),
-		[]byte("(((pos| end of pri msg.. |pos)))"),
-	)
+	pid := os.Getpid()
+	fmt.Printf("Running under PID: %d\n\n", pid)
 	fmt.Println("Press enter after running 'memory_reader.py' to exit...")
 	line := ""
 	fmt.Scanln(&line)
-	fmt.Printf("Pub\nRaw byte output: %v\nString output: %s\n\n", publicMsg, publicMsg)
-	fmt.Printf("Pri\nRaw byte output: %v\nString output: %s\n\n", privateMsg, privateMsg)
-	fmt.Println(emptyMsg)
+	fmt.Printf("%T: %s\n", msg, msg)
 }
 
+// helper
 func assert32Bytes(in []byte) []byte {
 	if len(in) != 32 {
 		panic("invalid length")
